c9ffc6c8ed2ed296cf36c2af97435f9bd40e04cf
Frank Simon <f.m.sim@t-online.de>
2026-06-03 19:31:57 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,19 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// Middleware order is important. CORS should be before authorization and authentication.
+app.UseCors(
+    (opt) =>
+    {
+        opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+    }
+);
+
 app.MapControllers();
 
 DbInitializer.InitDb(app);
